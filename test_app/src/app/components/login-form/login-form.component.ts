156008dab7d824044cf6ecdada23cb27b0309572
Alejandro Inigo Portugues <aleks.ip23@gmail.com>
2023-03-21 21:31:34 +0100

    Thecnical test according to design

--- test_app/src/app/components/login-form/login-form.component.ts
+++ test_app/src/app/components/login-form/login-form.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class LoginFormComponent {
 
+  email = '';
+  password = '';
+  remember = false;
+  showFormErrors = false;
+  emailErrorMessage = '';
+  passwordErrorMessage = '';
+
+  public onSubmit(form: any) {
+    if (this.validateForm(form)) {
+      console.log('OK');
+    } else {
+      this.showFormErrors = true;
+    }
+  }
+
+  private validateForm(form: any): boolean {
+    console.log(form.controls);
+    this.emailErrorMessage = '';
+    this.passwordErrorMessage = '';
+    let validate = true;
+    if (form.controls.email.errors && form.controls.email.errors.required) {
+      this.emailErrorMessage = 'El campo es obligatorio';
+      validate = false;
+    } else if (form.controls.email.errors && form.controls.email.errors.email) {
+      this.emailErrorMessage = 'El email introducido no es válido';
+      validate = false;
+    }
+    if (form.controls.password.errors && form.controls.password.errors.required) {
+      this.passwordErrorMessage = 'El campo es obligatorio';
+      validate = false;
+    } else if (form.controls.password.value.length < 5) {
+      this.passwordErrorMessage = 'La contraseña tiene que ser mínimo de 5 caracteres'
+      validate = false;
+    }
+    return validate;
+  }
 }
